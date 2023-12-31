4247ac8cce527c59436033f51fe9a219dac50ae7
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-31 01:34:40 -0600

    fix(root): standardize charset and viewport meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -17,8 +17,8 @@ export default function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Meta />
         <Links />
       </head>
