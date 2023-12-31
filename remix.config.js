cbc0c51c5fc21f8af0e863d89c2c5954f25bf660
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-31 15:02:28 -0600

    feat(remix-config): enable experimental v3 features

--- remix.config.js
+++ remix.config.js
@@ -3,5 +3,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   cacheDirectory: './node_modules/.cache/remix',
+  future: {
+    v3_fetcherPersist: true,
+    v3_relativeSplatPath: true,
+  },
   ignoredRouteFiles: ['**/.*'],
 };
