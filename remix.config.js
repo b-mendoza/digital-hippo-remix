51494f13190bb6205dd94585d02a7373958e3d01
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-31 15:01:34 -0600

    chore: standardize cache location
    Remove redundant .cache entries from .eslintignore, .gitignore, and .prettierignore as the cache directory is now specified in remix.config.js.

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,6 @@
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
 };
