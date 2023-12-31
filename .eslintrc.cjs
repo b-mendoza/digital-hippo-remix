ef9937ec8653e1873d3182d83325507ba233c6c5
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-31 04:01:31 -0600

    feat(eslint): enhance config with new plugins and stylistic rules
    This commit updates ESLint configuration to include additional plugins (`eslint-plugin-sonarjs` and `eslint-plugin-deprecation`). The `@typescript-eslint/consistent-type-definitions` and `@typescript-eslint/consistent-type-imports` rules are enforced to ensure consistency in type definitions and imports, while the `@typescript-eslint/explicit-function-return-type` rule is turned off for flexibility. Additionally, unnecessary strictness in `.d.ts` files is relaxed by turning off `@typescript-eslint/triple-slash-reference`.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,32 @@
 // @ts-check
 
 // Severity Types
-// const OFF = 0;
+const OFF = 0;
 // const WARN = 1;
 const ERROR = 2;
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
     'standard-with-typescript',
+    'plugin:sonarjs/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/strict',
+    'plugin:deprecation/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   parserOptions: {
     project: ['./tsconfig.json', './tsconfig.eslint.json'],
     tsconfigRootDir: __dirname,
@@ -29,12 +36,7 @@ module.exports = {
       jsx: true,
     },
   },
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-  },
-  root: true,
+  plugins: ['@typescript-eslint'],
   settings: {
     react: {
       version: 'detect',
@@ -46,6 +48,20 @@ module.exports = {
     ],
   },
   rules: {
+    '@typescript-eslint/consistent-type-definitions': [ERROR, 'type'],
+    '@typescript-eslint/consistent-type-imports': [
+      ERROR,
+      { fixStyle: 'separate-type-imports' },
+    ],
+    '@typescript-eslint/explicit-function-return-type': OFF,
     'react/jsx-no-leaked-render': [ERROR, { validStrategies: ['ternary'] }],
   },
+  overrides: [
+    {
+      files: ['**/*.d.ts'],
+      rules: {
+        '@typescript-eslint/triple-slash-reference': OFF,
+      },
+    },
+  ],
 };
