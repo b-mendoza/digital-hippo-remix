e7d6b569bf0479465b065e47d8298f94ce3c2262
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-31 18:10:08 -0600

    feat(max-width-wrapper): add MaxWidthWrapper component

---
+++ app/components/max-width-wrapper/max-width-wrapper.component.tsx
@@ -0,0 +1,19 @@
+import { cn } from '~/lib/utils';
+
+type MaxWidthWrapperProps = React.JSX.IntrinsicElements['div'];
+
+export const MaxWidthWrapper = (props: MaxWidthWrapperProps) => {
+  const { children, className, ...restOfProps } = props;
+
+  return (
+    <div
+      className={cn(
+        'mx-auto w-full max-w-screen-xl px-2.5 sm:px-20',
+        className,
+      )}
+      {...restOfProps}
+    >
+      {children}
+    </div>
+  );
+};
